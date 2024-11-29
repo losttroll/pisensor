16e369089168979a328896543b43b2bedd5e6700
losttroll <lost-troll@outlook.com>
2024-11-29 09:41:19 -0700

    Adding weekly and daily scripts

---
+++ daily.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+
+#0 0 * * *  sudo /usr/bin/bash $INSTALLDIR/daily.sh > /dev/null
---
+++ weekly.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+
+#0 0 * * 0  sudo /usr/bin/bash $INSTALLDIR/weekly.sh > /dev/null
+
